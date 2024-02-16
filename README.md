cf605b25f948a50f09e62eb2e20233d84011e8e1
Jean-Marc Barozet <jbarozet@gmail.com>
2024-02-16 10:41:46 +0100

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# openchat
+
+A very simple example to provide a ChatGPT-like interface, but running locally on your PC/Mac, and using ollama.
+
+Start ollama server:
+
+```console
+ollama serve
+```
+
+Run ollama with mistral model:
+
+```console
+ollama run mistral
+```
+
+Then run the script, which will launch a web interface:
+
+```console
+python3 openchat.py
+```
---
+++ openchat.py
@@ -0,0 +1,57 @@
+# ------------------------------------------------------------------------------
+#   ___  _     _        _    __  __    _
+#  / _ \| |   | |      / \  |  \/  |  / \
+# | | | | |   | |     / _ \ | |\/| | / _ \
+# | |_| | |___| |___ / ___ \| |  | |/ ___ \
+#  \___/|_____|_____/_/   \_\_|  |_/_/   \_\
+#
+#
+#  Updated: jmb (2024-02)
+#
+# ------------------------------------------------------------------------------
+
+import json
+
+import gradio as gr
+import requests
+
+url = "http://localhost:11434/api/generate"
+
+headers = {
+    "Content-Type": "application/json",
+}
+
+conversation_history = []
+
+
+def generate_response(prompt):
+    conversation_history.append(prompt)
+
+    full_prompt = "\n".join(conversation_history)
+
+    data = {
+        "model": "mistral",
+        "stream": False,
+        "prompt": full_prompt,
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        response_text = response.text
+        data = json.loads(response_text)
+        actual_response = data["response"]
+        conversation_history.append(actual_response)
+        return actual_response
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
+
+
+iface = gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(lines=2, placeholder="Enter your prompt here..."),
+    outputs="text",
+)
+
+iface.launch()
