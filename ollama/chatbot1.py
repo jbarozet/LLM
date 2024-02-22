3a03ac52af49b9cb4788efb0018d0bab324d0e4a
Jean-Marc Barozet <jbarozet@gmail.com>
2024-02-22 14:32:30 +0100

    add 2 chatbots based on ollama

---
+++ ollama/chatbot1.py
@@ -0,0 +1,41 @@
+# Description: run a chatbot using Ollama and local model
+# Usage: python chatbot1.py
+# This will display a URL that you can use to chat
+#
+# Last Updated: 2024-02-22
+
+from langchain_community.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+import gradio as gr
+
+
+# Conversation
+conversation_history = []
+
+# LLM parameters
+temperature = 0.75
+max_tokens = 2000
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+# Load a model using ollama
+
+llm = Ollama(model="llama2", model_path=model_path, temperature=temperature, max_tokens=max_tokens, callback_manager=callback_manager)
+
+print(f"Loaded LLM model {llm.model}")
+
+def generate_response(prompt):
+    conversation_history.append(prompt)
+    full_prompt = "\n".join(conversation_history)
+    output = llm.invoke(full_prompt)
+    return output
+
+# Load graphical interface
+iface = gr.Interface(
+    fn=generate_response,
+    inputs=gr.Textbox(lines=2, placeholder="Enter your prompt here..."),
+    outputs="text",
+)
+
+iface.launch()
---
+++ ollama/chatbot2.py
@@ -0,0 +1,43 @@
+# Description: run a chatbot using Ollama and local model
+# Usage: streamlit run chatbot2.py
+#
+# Last Updated: 2024-02-22
+# 
+from langchain_community.llms import Ollama
+from langchain_community.document_loaders import WebBaseLoader
+# from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.chains import RetrievalQA
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+import gradio as gr
+import streamlit as st 
+
+
+# Conversation
+conversation_history = []
+
+# LLM parameters
+model_path = "/Users/jbarozet/LLM/llama-2-7b-chat.Q4_K_M.gguf"
+# odel_path="/Users/jbarozet/LLM/synthia-7b-v2.0-16k.Q4_K_M.gguf"
+
+# Load a model using ollama
+llm = Ollama(model="llama2")
+print(f"Loaded LLM model {llm.model}")
+
+colA, colB = st.columns([.90, .10])
+
+with colA:
+    prompt = st.text_input("prompt", value="", key="prompt")
+response = ""
+
+with colB:
+    st.markdown("")
+    st.markdown("")
+    if st.button("👉", key="button"):
+        response = llm.invoke(prompt)
+
+st.markdown(response)
